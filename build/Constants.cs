d54d8f42897432dffad5705edbae3f252e6912a1
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2019-07-22 08:54:19 +0200

    push source

--- build/Constants.cs
+++ build/Constants.cs
@@ -2,6 +2,7 @@ partial class BuildTargets : Amg.Build.Targets
 {
     string name => "Amg.Build";
     string company => "Amg";
-    string nugetPushSource => @"C:\src\local-nuget-repository";
+    // string nugetPushSource => @"C:\src\local-nuget-repository";
+	string nugetPushSource => @"https://captain.rtf.siemens.net/artifactory/api/nuget/chp-release-nuget"
     string nugetPushSymbolSource => nugetPushSource;
 }
