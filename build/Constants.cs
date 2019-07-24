84f731286344ae378ea1309a050b5fb16f3229f5
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2019-07-24 07:58:27 +0200

    nuget source

--- build/Constants.cs
+++ build/Constants.cs
@@ -3,6 +3,6 @@ partial class BuildTargets : Amg.Build.Targets
     string name => "Amg.Build";
     string company => "Amg";
     // string nugetPushSource => @"C:\src\local-nuget-repository";
-	string nugetPushSource => @"https://pkgs.dev.azure.com/CommonHostPlatform/_packaging/chp/nuget/v3/index.json";
+	string nugetPushSource => @"default";
     string nugetPushSymbolSource => nugetPushSource;
 }
