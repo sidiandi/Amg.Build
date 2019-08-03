6cd8b7949a0f3bfbb9d9c16d308a89a1b9c6bdfb
Andreas Grimme <andreas.grimme@siemens.com>
2019-08-03 10:37:14 +0200

    summary with timelines

---
+++ src/Amg.Build/Summary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Amg.Build
+{
+    internal class Summary
+    {
+        internal static IWritable Print(IEnumerable<OnceInterceptor.Invocation> invocations) => TextFormatExtensions.GetWritable(@out =>
+        {
+            var begin = invocations
+                .Select(_ => _.Begin.GetValueOrDefault(DateTime.MaxValue))
+                .Min();
+
+            var end = invocations
+                .Select(_ => _.End.GetValueOrDefault(DateTime.MinValue))
+                .Max();
+
+            invocations.OrderBy(_ => _.End)
+                .Select(_ => new
+                {
+                    Name = _.Id,
+                    State = _.State,
+                    Duration = _.Duration.HumanReadable(),
+                    Timeline = TextFormatExtensions.TimeBar(80, begin, end, _.Begin.Value, _.End.Value)
+                })
+                .ToTable()
+                .Write(@out);
+        });
+    }
+}
