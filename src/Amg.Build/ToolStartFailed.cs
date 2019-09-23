bf847422327701df398a3558cd9192a890c15581
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-23 05:46:51 +0200

    frequently used tools

---
+++ src/Amg.Build/ToolStartFailed.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Amg.Build
+{
+    /// <summary>
+    /// To be thrown by ITool.Run when the process cannot be started.
+    /// </summary>
+    [Serializable]
+    public class ToolStartFailed : Exception
+    {
+        /// <summary>
+        /// information that was used to start the process.
+        /// </summary>
+        public ProcessStartInfo StartInfo { get; }
+
+        /// <summary />
+        public ToolStartFailed()
+        {
+        }
+
+        /// <summary />
+        public ToolStartFailed(string message, ProcessStartInfo startInfo)
+            : base(message)
+        {
+            StartInfo = startInfo;
+        }
+
+        /// <summary />
+        protected ToolStartFailed(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        static string GetPath(ProcessStartInfo startInfo)
+        {
+            return startInfo.EnvironmentVariables["PATH"]
+                .Split(';')
+                .Join();
+        }
+
+        /// <summary />
+        public override string Message => $@"{base.Message}
+{new
+        {
+            this.StartInfo.FileName,
+            this.StartInfo.Arguments,
+            StartInfo.WorkingDirectory,
+            StartInfo.UserName,
+            Path = GetPath(StartInfo),
+            Environment = StartInfo.EnvironmentVariables.Cast<System.Collections.DictionaryEntry>()
+                .Select(_ => $"set {_.Key}={_.Value}").Join(),
+        }.Dump()}";
+
+        /// <summary />
+        public string DiagnosticMessage => $@"{base.Message}
+{new
+        {
+            this.StartInfo.FileName,
+            this.StartInfo.Arguments,
+            StartInfo.UserName,
+            Path = GetPath(StartInfo),
+            StartInfo.WorkingDirectory,
+            Environment = StartInfo.EnvironmentVariables.Cast<System.Collections.DictionaryEntry>()
+                .Select(_ => $"set {_.Key}={_.Value}").Join(),
+        }.Dump()}";
+    }
+}
---
+++ src/Amg.Build/Tools.cs
@@ -0,0 +1,13 @@
+﻿namespace Amg.Build
+{
+    /// <summary>
+    /// Frequently used tools
+    /// </summary>
+    public class Tools
+    {
+        /// <summary>
+        /// cmd.exe /c
+        /// </summary>
+        public static ITool Cmd => new Tool("cmd.exe").WithArguments("/c");
+    }
+}
