c0b7af6feba00d87b8898f8d02fd13109858866e
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-24 05:50:03 +0200

    test

---
+++ src/Amg.Build.Tests/SourceCodeLayoutTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amg.Build
+{
+    [TestFixture]
+    class SourceCodeLayoutTests : TestBase
+    {
+        private static readonly Serilog.ILogger Logger = Serilog.Log.Logger.ForContext(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Test]
+        public async Task Get()
+        {
+            var testDir = CreateEmptyTestDirectory();
+            var name = "hello";
+            await testDir.Combine(name + ".cmd").WriteAllTextAsync(String.Empty);
+            var srcDir = testDir.Combine(name);
+            await srcDir.Combine(name + ".csproj").WriteAllTextAsync(String.Empty);
+            await srcDir.Combine(name + ".cs").WriteAllTextAsync(String.Empty);
+            var dll = await srcDir.Combine("bin", "debug", "netcoreapp2.1", name + ".dll").WriteAllTextAsync(String.Empty);
+
+            var s = SourceCodeLayout.Get(dll);
+            Assert.That(s != null);
+
+            await s.Check();
+        }
+    }
+}
