edf7f33900517b819a91df4a1781d5c785a5514e
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2019-09-27 09:55:30 +0200

    netcoreapp2.1 everywhere

--- src/Amg.Build/build.cmd
+++ src/Amg.Build/build.cmd
@@ -1,7 +1,8 @@
 @echo off
+rem Do not modify below this line
 rem Bootstrapper script of Amg.Build
-rem Do not modify.
 rem See https://github.com/sidiandi/Amg.Build
+rem 
 setlocal EnableDelayedExpansion
 set buildDll=%~dp0%~n0\bin\Debug\netcoreapp2.1\build.dll
 set exitCodeRebuildRequired=2
