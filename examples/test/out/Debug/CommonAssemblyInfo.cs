658ad8a61a82543e426fa54f3ffb3027ef40d8b3
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2019-09-27 11:16:15 +0200

    amgbuild

--- examples/test/out/Debug/CommonAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Generated. Changes will be lost.
-[assembly: System.Reflection.AssemblyCopyright("Copyright (c) Amg 2019")]
-[assembly: System.Reflection.AssemblyCompany("Amg")]
-[assembly: System.Reflection.AssemblyProduct("Amg.Build")]
-[assembly: System.Reflection.AssemblyVersion("0.1.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.1.0-ci.46+Branch.master.Sha.a4bd763ac968602768f0a645783bd5b189ed4ed7")]
