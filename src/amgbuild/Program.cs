a2ab0a026c634881f6141c4648a239d7de86b24a
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2019-09-27 10:28:56 +0200

    amgbuild

---
+++ src/amgbuild/Program.cs
@@ -0,0 +1,24 @@
+﻿using Amg.Build;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+namespace amgbuild
+{
+    internal class Program
+    {
+        private static readonly Serilog.ILogger Logger = Serilog.Log.Logger.ForContext(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static int Main(string[] args) => Runner.Run(args);
+
+        [Once]
+        [Description("Create an Amg.Build script")]
+        public virtual async Task Init(string name)
+        {
+            var sourceLayout = await Amg.Build.SourceCodeLayout.Create(name);
+        }
+    }
+}
