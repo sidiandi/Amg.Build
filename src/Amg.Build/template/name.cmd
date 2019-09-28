0ac4bbc08d21735603a84c902de67c25f970c5df
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-28 07:19:02 +0200

    cmd

--- src/Amg.Build/template/name.cmd
+++ src/Amg.Build/template/name.cmd
@@ -1,6 +1,7 @@
 @echo off
 rem Do not modify below this line
 rem Bootstrapper script of https://github.com/sidiandi/Amg.Build
-set project=%~dpn0
-set fastrun=%project%\out\fastrun.cmd
-if exist %fastrun% ( %fastrun% run --project %project% -- %* ) else ( dotnet run --project %project - %* )
+set name=%~n0
+set dll=%~dp0%name%\bin\Debug\netcoreapp2.1\%name%.dll
+set project=%~dp0%name%\%name%.csproj
+if exist %dll% ( dotnet %dll% -- %* ) else ( dotnet run --project %project% - %* )
