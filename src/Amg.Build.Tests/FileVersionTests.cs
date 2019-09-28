df624727dc912af07f7725cc97758485915c06c5
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-28 18:48:18 +0200

    cmd

---
+++ src/Amg.Build.Tests/FileVersionTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Amg.Build
+{
+    [TestFixture]
+    public class FileVersionTests
+    {
+        [Test]
+        public void DllIsNewerThanSourceCode()
+        {
+            var dll = Assembly.GetExecutingAssembly().Location;
+            var sourceDir = dll.Parent().Parent().Parent().Parent();
+            Assert.That(FileVersion.Get(dll).IsNewer(FileVersion.Get(sourceDir)));
+        }
+    }
+}
