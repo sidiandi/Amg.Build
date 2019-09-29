0a34a67818ef724fef7a572d3c6821f850017542
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-29 07:43:23 +0200

    all tests green

---
+++ src/Amg.Build.Tests/InvocationInfoTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amg.Build
+{
+    [TestFixture]
+    class InvocationInfoTests
+    {
+        [Test]
+        public void GetResultType()
+        {
+            Assert.That(!InvocationInfo.TryGetResultType(Task.CompletedTask, out var resultType));
+        }
+
+        [Test]
+        public void GetResultType2()
+        {
+            Assert.That(InvocationInfo.TryGetResultType(Task.FromResult(String.Empty), out var resultType));
+            Assert.That(resultType, Is.EqualTo(typeof(string)));
+        }
+    }
+}
