1f766ec418996527f39762b7b1be02a6d0625d88
Andreas Grimme <andreas.grimme@siemens.com>
2019-09-30 06:01:26 +0200

    TargetFramework

--- src/Amg.Build/template/name.cmd
+++ src/Amg.Build/template/name.cmd
@@ -1,9 +1,9 @@
-@echo off
+rem @echo off
 rem Do not modify below this line
 rem Bootstrapper script of https://github.com/sidiandi/Amg.Build
 setlocal
-set TargetFramework=netcoreapp3.0
+set AmgBuildTargetFramework=netcoreapp3.0
 set name=%~n0
-set dll=%~dp0%name%\bin\Debug\%TargetFramework%\%name%.dll
+set dll=%~dp0%name%\bin\Debug\%AmgBuildTargetFramework%\%name%.dll
 set project=%~dp0%name%\%name%.csproj
 if exist %dll% ( dotnet %dll% %* ) else ( dotnet run --project %project% - %* )
