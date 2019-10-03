027c84bdf0d3dc3b888f307686bb0bf437476856
Andreas Grimme <andreas.grimme@siemens.com>
2019-10-03 05:13:02 +0200

    Sonar finding free

---
+++ src/Amg.Build/CommandLine/ParserException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Amg.CommandLine
+{
+    [Serializable]
+#pragma warning disable S3871 // Exception types should be "public"
+    internal class ParserException : Exception
+#pragma warning restore S3871 // Exception types should be "public"
+    {
+        public ParserException()
+        {
+        }
+
+        public ParserException(string message) : base(message)
+        {
+        }
+
+        public ParserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ParserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Amg.Build/ToolStartException.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Amg.Build
+{
+    /// <summary>
+    /// To be thrown by ITool.Run when the process cannot be started.
+    /// </summary>
+    [Serializable]
+    public class ToolStartException : Exception
+    {
+        /// <summary>
+        /// information that was used to start the process.
+        /// </summary>
+        public ProcessStartInfo StartInfo { get; }
+
+        /// <summary />
+        public ToolStartException(string message, ProcessStartInfo startInfo)
+            : base(message)
+        {
+            StartInfo = startInfo;
+        }
+
+        /// <summary />
+        protected ToolStartException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            StartInfo = null!;
+        }
+
+        static string GetPath(ProcessStartInfo startInfo)
+        {
+            return startInfo.EnvironmentVariables["PATH"]
+                .Split(';')
+                .Join();
+        }
+
+        /// <summary />
+        public override string Message => $@"{base.Message}
+{new
+        {
+            this.StartInfo.FileName,
+            this.StartInfo.Arguments,
+            StartInfo.WorkingDirectory,
+            StartInfo.UserName,
+            Path = GetPath(StartInfo),
+            Environment = StartInfo.EnvironmentVariables.Cast<System.Collections.DictionaryEntry>()
+                .Select(_ => $"set {_.Key}={_.Value}").Join(),
+        }.Dump()}";
+
+        /// <summary />
+        public string DiagnosticMessage => $@"{base.Message}
+{new
+        {
+            this.StartInfo.FileName,
+            this.StartInfo.Arguments,
+            StartInfo.UserName,
+            Path = GetPath(StartInfo),
+            StartInfo.WorkingDirectory,
+            Environment = StartInfo.EnvironmentVariables.Cast<System.Collections.DictionaryEntry>()
+                .Select(_ => $"set {_.Key}={_.Value}").Join(),
+        }.Dump()}";
+    }
+}
