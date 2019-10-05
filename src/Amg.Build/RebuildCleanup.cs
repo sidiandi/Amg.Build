cc29ef5d2ea87a8b9c3b594b7b3c2e4bbea3b002
Andreas Grimme <andreas.grimme@siemens.com>
2019-10-05 07:35:00 +0200

    log rebuild cleanup

--- src/Amg.Build/RebuildCleanup.cs
+++ src/Amg.Build/RebuildCleanup.cs
@@ -84,7 +84,7 @@ namespace Amg.Build
             var si = new ProcessStartInfo
             {
                 FileName = fileName,
-                CreateNoWindow = true,
+                CreateNoWindow = false,
                 UseShellExecute = false
             };
 
