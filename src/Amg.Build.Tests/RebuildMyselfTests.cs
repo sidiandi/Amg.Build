dcf598fea84a9f619c6de1b677ad7fac1581994b
Andreas Grimme <andreas.grimme@siemens.com>
2019-10-06 09:09:53 +0200

    remove unit test

--- src/Amg.Build.Tests/RebuildMyselfTests.cs
+++ src/Amg.Build.Tests/RebuildMyselfTests.cs
@@ -7,17 +7,5 @@ namespace Amg.Build
     [TestFixture]
     public class RebuildMyselfTests : TestBase
     {
-        [Test]
-        public async Task Rebuild()
-        {
-            var testDir = CreateEmptyTestDirectory();
-            var cmd = testDir.Combine("hello.cmd");
-            var layout = await SourceCodeLayout.Create(cmd);
-            var s = RebuildMyself.GetSourceInfo(layout.DllFile)!;
-            var version = (await FileVersion.Get(s.SourceDir))!;
-            var assembly = await RebuildMyself.ProvideAssembly(s, version);
-            Console.WriteLine(s.SourceDir.Glob("**/*").EnumerateFiles().Join());
-            Assert.That(assembly.IsFile(), () => s.Dump().ToString());
-        }
     }
 }
