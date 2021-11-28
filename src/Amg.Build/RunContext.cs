dcc1dba6393aa75fc940e7a0a4116eaf530c7563
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2021-11-28 15:06:50 +0100

    fix build

--- src/Amg.Build/RunContext.cs
+++ src/Amg.Build/RunContext.cs
@@ -157,7 +157,7 @@ namespace Amg.Build
                 }
 
                 var amgBuildAssembly = Assembly.GetExecutingAssembly();
-                Logger.Debug("{name} {version}", amgBuildAssembly.GetName().Name, amgBuildAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().Version);
+                Logger.Debug("{name} {version}", amgBuildAssembly.GetName().Name, amgBuildAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
 
                 try
                 {
